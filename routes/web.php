078c9fd2aec3858632185155fc40f0de1e581423
Your Name <you@example.com>
2018-07-02 15:06:12 +0800

    xuhuanle提交

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//后台路由组
+Route::group([],function() {
+
+	Route::any('admin/index','admin\IndexController@index');
+	Route::any('admin/ad/add','admin\ad\AdController@create');
+	Route::any('admin/ad/index','admin\ad\AdController@index');
+});
